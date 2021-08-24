d0592a280f8d33ba1e3c1ad6eba7b3c6f98dc118
BunFrith <bunfrith.dev@gmail.com>
2021-08-24 17:34:50 +0300

    first simple component

--- training/src/App.vue
+++ training/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList :todos="todos" @delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      todos: [ 'Ремонт', 'Уборка' ],
+    }
+  },
   components: {
-    HelloWorld
-  }
+    ToDoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ training/src/components/ToDoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todos" :key="index" @click="deleteTodo(index)">{{ todo }}</li>
+    </ul>
+</template>
+
+
+<script>
+export default {
+    props: ['todos'],
+    methods: {
+        deleteTodo(index){
+            this.$emit('delete-todo', index);
+        }
+    }
+}
+</script>
